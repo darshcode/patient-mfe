64910eedeae7d211b7ced26ad254fc97453a896d
Darshmeen Dhillon <darsh.dhillon@live.com>
2025-04-09 12:53:08 -0400

    bla

--- patient-app/vite.config.js
+++ patient-app/vite.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+
 export default defineConfig({
   plugins: [
     react(),
